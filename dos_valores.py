f94e5cefb554ea50af68f99c43ddf83e6c650fd3
dpurpop0909 <dpurpop0909@g.educaand.es>
2025-04-01 09:49:22 +0200

    ters

--- dos_valores.py
+++ dos_valores.py
@@ -2,8 +2,9 @@
 # Y producir una salida en orden creciente.
 
 n = [15,23,35]
-if n[0] > n[1]:
+if n[0] > n[1] > n[2]:
     a = n[0]
-    n[0] = n[1]
+    n[0] = n[1] = n[2]
     n[1] = a
+    n[2] = a
 print(n)
